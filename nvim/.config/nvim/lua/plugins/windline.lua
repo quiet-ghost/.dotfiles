b511556be940d535f4d567b2e3532f28be5bc12d
Kevin Sclafani <145733929+quiet-ghost@users.noreply.github.com>
2025-12-11 18:28:33 -0500

    feat: rose-pine theme across scripts

--- nvim/.config/nvim/lua/plugins/windline.lua
+++ nvim/.config/nvim/lua/plugins/windline.lua
@@ -9,15 +9,15 @@ return {
       opts.colors_name = function(colors)
         -- Rose Pine Moon theme colors
         colors.magenta = "#c4a7e7" -- iris
-        colors.blue = "#9ccfd8"    -- foam
-        colors.white = "#e0def4"   -- text
-        colors.green = "#3e8fb0"   -- pine
-        colors.red = "#eb6f92"     -- love
-        colors.yellow = "#f6c177"  -- gold
-        colors.cyan = "#9ccfd8"    -- foam
-        colors.orange = "#ea9a97"  -- rose
-        colors.black = "#232136"   -- base
-        colors.gray = "#908caa"    -- muted
+        colors.blue = "#9ccfd8" -- foam
+        colors.white = "#e0def4" -- text
+        colors.green = "#3e8fb0" -- pine
+        colors.red = "#eb6f92" -- love
+        colors.yellow = "#f6c177" -- gold
+        colors.cyan = "#9ccfd8" -- foam
+        colors.orange = "#ea9a97" -- rose
+        colors.black = "#191724" -- base
+        colors.gray = "#908caa" -- muted
         return colors
       end
       return original_setup(opts)
