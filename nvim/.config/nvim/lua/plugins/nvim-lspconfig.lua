319308118e997bf9a4a4524bb53e3e7ca3b3eacb
Kevin Sclafani <145733929+quiet-ghost@users.noreply.github.com>
2026-02-08 13:17:22 -0500

    fix: go with mise/mason

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,9 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     setup = {
-      gopls = function(_, opts)
-        opts.mason = false
-      end,
       jdtls = function()
         return true
       end,
