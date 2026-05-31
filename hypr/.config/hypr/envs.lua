966b13002720a14b1c0528e185dfae4b3d079433
K <145733929+quiet-ghost@users.noreply.github.com>
2026-05-30 22:19:17 -0400

    update: hyprland config changes for lua (#1)
    * update: hyprland config changes for lua
    
    * fix: removing .conf

---
+++ hypr/.config/hypr/envs.lua
@@ -0,0 +1,11 @@
+-- Extra environment variables. Hyprland restart required after changes.
+
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("HYPRCURSOR_SIZE", "20")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("XCURSOR_SIZE", "20")
+
+-- NVIDIA environment variables.
+hl.env("NVD_BACKEND", "direct")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
