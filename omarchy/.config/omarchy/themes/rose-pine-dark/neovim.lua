696a17014e689863a38a72e0d0fe31a241cb6b69
Kevin <145733929+quiet-ghost@users.noreply.github.com>
2026-07-27 07:41:21 -0400

    chore(omarchy): vendor rose pine theme

---
+++ omarchy/.config/omarchy/themes/rose-pine-dark/neovim.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		-- Customize theme specs, e.g: disable italic. For full spec: https://github.com/rose-pine/neovim
+		-- config = function()
+		-- require("rose-pine").setup({
+		--  styles = {
+		--    italic = false,
+		--  },
+		-- })
+		-- end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine",
+		},
+	},
+}
